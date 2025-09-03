4f36e56e6d05e220d1d97eca412e54c4a586deb2
omar-hail <donz.h.h@gmail.com>
2025-09-03 00:53:36 +0000

    task0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates src to the end of dest
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
