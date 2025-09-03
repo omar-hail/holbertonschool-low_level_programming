1aa423b48124af530160a0f161bbe2415a9a3c5f
omar-hail <donz.h.h@gmail.com>
2025-09-03 00:14:24 +0000

    task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: pointer to destination buffer
+ * @src: pointer to source string
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+
+	return (dest);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,5 +25,7 @@ void puts2(char *str);
 void puts_half(char *str);
 /* 8-print_array.c */
 void print_array(int *a, int n);
+/* 9-strcpy.c */
+char *_strcpy(char *dest, char *src);
 
 #endif /* MAIN_H */
