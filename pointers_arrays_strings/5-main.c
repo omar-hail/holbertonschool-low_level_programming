197cd5d1b154d4d81784909e00e21328b3ec6add
omar-hail <donz.h.h@gmail.com>
2025-09-08 10:01:38 +0000

    task 5

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -1,16 +1,44 @@
-#include "main.h"
 #include <stdio.h>
+#include "holberton.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+char *_strstr(char *haystack, char *needle)
+{
+    int a = 0, b = 0;
+
+    while (haystack[a])
+    {
+        b = 0;
+        while (needle[b])
+        {
+            if (haystack[a + b] != needle[b])
+            {
+                break;
+            }
+            b++;
+        }
+
+        if (needle[b] == '\0')
+        {
+            return (haystack + a);
+        }
+
+        a++;
+    }
+
+    return (NULL);
+}
+
 int main(void)
 {
-    char str[] = "Hi catwoman";
-    
-    printf("%s\n", string_toupper(str));
-    
-    return (0);
+    char haystack[] = "Holberton School is awesome!";
+    char needle[] = "School";
+
+    char *result = _strstr(haystack, needle);
+
+    if (result != NULL)
+        printf("Substring found at: %s\n", result);
+    else
+        printf("Substring not found.\n");
+
+    return 0;
 }
