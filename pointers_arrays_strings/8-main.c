402795389d50c25ce07cee2ba85f340a210f75f6
omar-hail <donz.h.h@gmail.com>
2025-09-08 10:10:33 +0000

    task 8

--- pointers_arrays_strings/8-main.c
+++ pointers_arrays_strings/8-main.c
@@ -1,19 +1,14 @@
-#include "main.h"
+#include "holberton.h"
 
-/**
- * main - check the code for
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    int array[5];
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
 
-    array[0] = 98;
-    array[1] = 402;
-    array[2] = -198;
-    array[3] = 298;
-    array[4] = -1024;
-    print_array(array, 5);
+    print_diagsums((int *)matrix, 3);
+
     return (0);
 }
