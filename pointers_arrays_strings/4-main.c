a54be2e0136e0e63a17fbe2fb77861c702375c5a
omar-hail <donz.h.h@gmail.com>
2025-09-08 09:56:50 +0000

    task 4

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -1,22 +1,42 @@
-#include "main.h"
 #include <stdio.h>
+#include "holberton.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+char *_strpbrk(char *s, char *accept)
+{
+    int a = 0, b;
+
+    while (s[a])
+    {
+        b = 0;
+
+        while (accept[b])
+        {
+            if (s[a] == accept[b])
+            {
+                s += a;
+                return (s);
+            }
+
+            b++;
+        }
+
+        a++;
+    }
+
+    return (NULL);
+}
+
 int main(void)
 {
-    int arr[] = {1, 2, 3, 4, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int i;
+    char str[] = "hello, world";
+    char accept[] = "ow";
 
-    reverse_array(arr, n);
+    char *result = _strpbrk(str, accept);
 
-    for (i = 0; i < n; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+    if (result != NULL)
+        printf("First accepted character found at: %s\n", result);
+    else
+        printf("No accepted characters found.\n");
 
-    return (0);
+    return 0;
 }
