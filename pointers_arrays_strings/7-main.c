b6a7a0dbd8aa641aad641871e07eadf3f01611d1
omar-hail <donz.h.h@gmail.com>
2025-09-08 10:07:37 +0000

    task 7

--- pointers_arrays_strings/7-main.c
+++ pointers_arrays_strings/7-main.c
@@ -1,16 +1,39 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
+int _putchar(char c)
+{
+    return putchar(c);
+}
+
+void print_chessboard(char (*a)[8])
+{
+    int b, c;
+
+    for (b = 0; b < 8; b++)
+    {
+        for (c = 0; c < 8; c++)
+        {
+            _putchar(a[b][c]);
+        }
+        _putchar('\n');
+    }
+}
+
 int main(void)
 {
-    char str[] = "are you real";
+    char board[8][8] = {
+        {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+        {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+        {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}
+    };
 
-    printf("%s\n", leet(str));
+    print_chessboard(board);
 
-    return (0);
+    return 0;
 }
