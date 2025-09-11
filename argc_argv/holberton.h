d8cd3fa999703f42648fd03f31e9e1af636df454
omar-hail <donz.h.h@gmail.com>
2025-09-11 22:11:12 +0000

    task 3

--- argc_argv/holberton.h
+++ argc_argv/holberton.h
@@ -4,6 +4,7 @@
 /* Add any function prototypes here if needed */
 /* No function prototypes needed for this program */
 /* No function prototypes needed for this program */
+/* No custom functions needed for this file */
 
 
 #endif /* HOLBERTON_H */
