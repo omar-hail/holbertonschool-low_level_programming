04ef5d55758e6327b8fdee3b5279847173e94782
omar-hail <donz.h.h@gmail.com>
2025-09-27 05:58:40 +0000

    task 0

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,11 +3,18 @@
 
 #include <stdarg.h>
 
+/**
+ * struct printer - Struct to match format specifiers with print functions
+ * @symbol: Pointer to the format specifier (e.g., "c", "i", "f", "s")
+ * @print: Pointer to function that handles printing for that specifier
+ *
+ * Description: Used in print_all to map format specifiers to their
+ * corresponding print handler functions.
+ */
 typedef struct printer
 {
 	char *symbol;
 	void (*print)(va_list arg);
-
 } printer_t;
 
 int sum_them_all(const unsigned int n, ...);
@@ -19,3 +26,4 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
+
